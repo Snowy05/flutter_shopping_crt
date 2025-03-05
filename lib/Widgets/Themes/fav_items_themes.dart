a5891c853c283559f8a12659618e209171899c89
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2025-03-05 11:47:53 +0000

    Added theme

---
+++ lib/Widgets/Themes/fav_items_themes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyFavItemsThemes{
+  ThemeData myFavItims = ThemeData(
+        cardTheme: CardTheme(color: Colors.grey,)
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context)=>FavItemNotifier(),
       child: MaterialApp(
+
         home: ShoppingListScreen(),
       ),
     );
